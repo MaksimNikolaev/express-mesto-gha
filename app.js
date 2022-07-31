5694fd9644c7a0e04f113e9fa80527fe5733ed30
Maksim Nikolaev <mksniklaev@gmail.com>
2022-08-01 00:01:37 +0500

    feat add models and routes

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { getUsers, getUsersById, createUser } = require('../controllers/users');
+
+router.get('/users', getUsers);
+router.get('/users/:userId', getUsersById);
+router.post('/users', createUser);
